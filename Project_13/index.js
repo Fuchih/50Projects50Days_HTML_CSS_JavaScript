8c980047cd78930fa81da123da4b821afd14d79c
FuChih <t19887348@hotmail.com>
2021-10-10 09:17:24 +0800

    Project_13 add modal

--- Project_13/index.js
+++ Project_13/index.js
@@ -1,21 +1,39 @@
+function showModal(val) {
+  const alertContent = document.querySelector('.alert')
+  const modal = document.querySelector('.modal')
+  const close = document.querySelector('#close')
+
+  modal.style.display = 'block'
+  alertContent.innerText = val
+
+  close.onclick = () => {
+    modal.style.display = 'none'
+  }
+}
+
 function checkID(formElement) {
   const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/
+
   if (!regex.test(formElement.value)) {
-    alert('UserID must contain: Minimum six characters, at least one letter and one number')
+    showModal('UserID must contain: Minimum six characters, at least one letter and one number')
     return
   }
 }
+
 function checkPwd(formElement) {
   const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+
   if (!regex.test(formElement.value)) {
-    alert('Passwords must contain: Minimum eight characters, at least one uppercase letter, one lowercase letter and one number')
+    showModal('Passwords must contain: Minimum eight characters, at least one uppercase letter, one lowercase letter and one number')
     return
   }
 }
+
 function confirmPwd(formElement) {
   const pwd = document.querySelector('#pwd')
+
   if (formElement.value != pwd.value) {
-    alert('Double check your password and try again')
+    showModal('Double check your password and try again')
     return
   }
 }
