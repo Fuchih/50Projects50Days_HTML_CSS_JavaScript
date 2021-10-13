27e8983561588ad5fd28fc680d05d2743b0883f8
FuChih <t19887348@hotmail.com>
2021-10-13 13:48:27 +0800

    p15 second commit

--- Project_15/index.js
+++ Project_15/index.js
@@ -2,16 +2,38 @@ const glasses = document.querySelectorAll('.glass')
 const empty = document.querySelector('.empty')
 const fulfil = document.querySelector('.fulfil ')
 const percent = document.querySelector('.percent ')
+const remain = document.querySelector('.remain ')
+const remained = document.querySelector('.remained ')
 
 glasses.forEach((glass, index) => {
   glass.onclick = () => {
-    glass.classList.toggle('color')
+    glass.classList.add('color')
 
-    if(index >= 0) fulfil.style.opacity = 1
-    // glass.style.opacity = 0
-
-    fulfil.style.height = `${12.5 * (index + 1)}%`
-    percent.innerText = `${12.5 * (index + 1)}%`
-    empty.style.height = `${100 - 12.5 * (index + 1)}%`
+    liter(index)
+    isFulfil(index)
+    glassColor(index)
   }
 })
+
+function isFulfil(index) {
+  empty.style.height = `${100 - 12.5 * (index + 1)}%`
+  fulfil.style.height = `${12.5 * (index + 1)}%`
+}
+
+function liter(index) {
+  remain.innerText = `${2 - (250 * (index + 1)) / 1000}L`
+  percent.innerText = `${12.5 * (index + 1)}%`
+
+  if (index === glasses.length - 1) {
+    remain.innerText = ''
+    remained.innerText = ''
+  } else {
+    remained.innerText = 'Remained'
+  }
+}
+
+function glassColor(index) {
+  for (let i = 0; i < index; i++) {
+    glasses[i].classList.add('color')
+  }
+}
